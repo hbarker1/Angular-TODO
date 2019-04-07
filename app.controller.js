245174ab281f75e45130c5b0c60586471dc3a814
hannah barker <hannah.m.barker@gmail.com>
2019-04-07 17:55:10 -0400

    added styling and fixed earlier glitch in controller

--- app.controller.js
+++ app.controller.js
@@ -19,27 +19,56 @@ function TodoController ($scope) {
           }
         ];
     
-        $scope.save (() => {
+        $scope.enter = (() => {
             let addTask = $scope.addTask;
             let completed = $scope.completed;
             let listItem = $scope.listItem;
             let removeItem = $scope.removeItem;
             
-                if (!$scope.addMe) {return;}
-                if ($scope.products.indexOf($scope.addMe) == -1) {
-                    $scope.products.push($scope.addMe);
-                } else {
-                    $scope.errortext = "The item is already in your shopping list.";
-                }
-           
-                $scope.errortext = "";    
-                $scope.todos.splice(listItem, 1, {
-                    text: addTask,
-                    completed: completed
-                });
-            
+                if (!addTask === undefined || !addTask == '') {
+                if (!listItem == '') {
+                    $scope.todos.splice(listItem, 1, {
+                        text: addTask,
+                        completed: completed
+                    });
+                }else {
+                    $scope.todos.push({
+                        text: addTask,
+                        complete: false
+                    });
+                };
+                           
+                $scope.addTask = $scope.completed = $scope.listItem = '';  
+        };
+    }); 
+    
+    $scope.remaining = function() {
+        let count = 0;
+        angular.forEach($scope.todos, function(task) {
+          count += task.completed ? 0 : 1;
         });
-    }    
+        return count;
+      };
+
+      $scope.clear = (() => {
+        let todos = $scope.todos;
+        
+        $scope.todos = [];
+        
+        angular.forEach(todos, function(task) {
+          if (!task.completed) $scope.todos.push(task);
+        });
+      });
+    
+      $scope.removeTask = (() => {
+        var oldList = $scope.todos;
+        $scope.todos = [];
+        angular.forEach(oldList, function(x) {
+            if (!x.completed) $scope.todos.push(x);
+        });
+      });
+    }
+    
 
 angular
   .module("todoApp")
