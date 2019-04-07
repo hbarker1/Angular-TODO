94806cd0281c5d2eab381c542775f391e95c75e2
hannah barker <hannah.m.barker@gmail.com>
2019-04-07 16:26:24 -0400

    using template to understand controller to html

--- app.controller.js
+++ app.controller.js
@@ -19,7 +19,22 @@ function TodoController ($scope) {
           }
         ];
     
-//  testing testing
+        $scope.save (() => {
+            $scope.products = ["Milk", "Bread", "Cheese"];
+            $scope.addItem = function () {
+                $scope.errortext = "";
+                if (!$scope.addMe) {return;}
+                if ($scope.products.indexOf($scope.addMe) == -1) {
+                    $scope.products.push($scope.addMe);
+                } else {
+                    $scope.errortext = "The item is already in your shopping list.";
+                }
+            }
+            $scope.removeItem = function (x) {
+                $scope.errortext = "";    
+                $scope.products.splice(x, 1);
+            }
+        });
 }
 angular
   .module("todoApp")
