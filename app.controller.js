41591cbca09251365b6d24922ab8ab6d4e48c517
hannah barker <hannah.m.barker@gmail.com>
2019-04-07 15:54:40 -0400

    starting Angular controller and added scope

--- app.controller.js
+++ app.controller.js
@@ -1,7 +1,27 @@
 "use strict";
-function MyCtrl($scope) {  
-  // Control the world
+function TodoController ($scope) {  
+    $scope.todos = [
+        {
+            text:'Walk the Dog',
+            completed: true
+          },
+          {
+            text:'Go to the store',
+            completed: false
+          },
+          {
+            text:'Get gas',
+            completed: false
+          },
+          {
+            text:'Wash the car',
+            completed: false
+          }
+        ];
+    
+ 
 }
 angular
   .module("App")
-  .controller("MyCtrl", MyCtrl);
+  .controller("TodoController", TodoController);
+  
